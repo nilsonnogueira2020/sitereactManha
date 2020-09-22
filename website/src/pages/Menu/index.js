faa9a2237203679c3c016527331741ceb40f813d
zevolpato <jose.avjunior@sp.senac.br>
2020-09-22 10:36:45 -0300

    commit

--- website/src/pages/Menu/index.js
+++ website/src/pages/Menu/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Logo from '../../assets/logo.png';
+import Banner from '../../assets/banner.png';
+
 export default function Menu(){
     return(
         <div className="container-menu">
+                <section>
+                    <img src={Logo} alt="selfcare"/>
+                </section>
                 <h1>MENU</h1>
 
+                <img src={Banner} alt="banner"/>
         </div>
     );
 }
