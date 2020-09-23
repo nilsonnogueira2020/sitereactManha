dc377103bb09e67c0e3b041dc9b8b784fd67c8b7
Nilson Nogueira <nilson.nogueira@gmail.com>
2020-09-23 03:10:39 -0300

    update footer index

--- website/src/pages/Footer/index.js
+++ website/src/pages/Footer/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-export default function Footer(){
-    return(
+import Visa from '../../assets/visa.png';
+import Master from '../../assets/master.png';
+import Pay from '../../assets/pay.png';
+
+
+export default function Footer() {
+    return (
         <div className="container-footer">
-            <h1>FOOTER</h1>
+            <section>
+                <form>
+                    <label>
+                        <h1> Newsletter </h1>
+                        <input type="text" size={40} color="green" />
+                    </label>
+                    <input type="submit" value="Pesquisar" alt="lupa" />
+                </form>
+                <img src={Visa} alt="visa" />
+                <img src={Master} alt="visa" />
+                <img src={Pay} alt="visa" />
+
+            </section>
+
         </div>
     );
 }
